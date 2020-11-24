ac5fec20ad7be8d71ce6dae7238d4f48362ced12
perivan13 <1and130699@gmail.com>
2020-11-24 19:36:43 +0200

    change addModel

--- project/backend/main/serializers.py
+++ project/backend/main/serializers.py
@@ -18,7 +18,6 @@ class ComponentSerializer(serializers.Serializer):
     class Meta:
         model = Component
         fields = "__all__"
-        #fields = ('name', 'serial_n', 'category', 'type', 'view', 'location', 'wealth')
     name = serializers.CharField(max_length=50)
     serial_n = serializers.CharField(max_length=50)
     category = serializers.CharField(max_length=50)
@@ -34,27 +33,6 @@ class EmployeeSerializer(serializers.ModelSerializer):
             'id',
         )
         fields = "__all__"
-    # id = serializers.ReadOnlyField()
-    # sirname = serializers.CharField(max_length=50)
-    # name = serializers.CharField(max_length=50)
-    # secname = serializers.CharField(max_length=50)
-    # position = serializers.CharField(max_length=50)
-    # rank = serializers.CharField(max_length=50)
-    # gender = serializers.CharField(max_length=50)
-    #
-    # def update(self, instance, validated_data):
-    #     instance.sirname = validated_data.get('sirname', instance.sirname)
-    #     instance.name = validated_data.get('name', instance.name)
-    #     instance.secname = validated_data.get('secname', instance.secname)
-    #     instance.position = validated_data.get('position', instance.position)
-    #     instance.rank = validated_data.get('rank', instance.rank)
-    #     instance.gender = validated_data.get('gender', instance.gender)
-    #     instance.save()
-    #     return instance
-    #
-    # def create(self, validated_data):
-    #     return Employee.objects.create(**validated_data)
-
 
 class ItemSerializer(serializers.ModelSerializer):
     class Meta:
