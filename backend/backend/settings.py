b3fcd55feb10405044747382d71fb2b16e5a0524
van4ester <1and130699@gmail.com>
2021-04-17 19:01:18 +0300

    cors_allowed

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -56,9 +56,11 @@ CORS_ALLOW_HEADERS = list(default_headers) + [
 CORS_ALLOWED_ORIGINS = (
     'http://localhost:8080',
     'http://127.0.0.1:8080',
+    'http://0.0.0.0:8080',
 
     'http://localhost:8000',
     'http://127.0.0.1:8000',
+    'http://0.0.0.0:8000',
     'http://sms.gitwork.ru'
 )
 
