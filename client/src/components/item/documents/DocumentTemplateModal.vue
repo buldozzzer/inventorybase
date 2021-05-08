b6cc5ef299c2f58a08a2deaf532631b215e13e79
van4ester <1and130699@gmail.com>
2021-05-08 21:49:18 +0300

    добавил предупреждение, если мат ценность для вставки в шаблон не выбрана

--- client/src/components/item/documents/DocumentTemplateModal.vue
+++ client/src/components/item/documents/DocumentTemplateModal.vue
@@ -3,6 +3,8 @@
            id="document-template-modal"
            title="Экспорт записей в шаблоны документов"
            centered
+           @hidden="clear"
+           @show="clear"
            hide-footer>
     <b-container>
       <b-row>
@@ -19,7 +21,7 @@
         </b-col>
         <b-col cols="4">
           <b-form-file plain
-                       accept=".docx, .xlsx"
+                       accept="application/msword, application/vnd.ms-excel"
                        v-model="file"></b-form-file>
         </b-col>
       </b-row>
@@ -36,6 +38,18 @@
             Слить документы в один
           </b-form-checkbox>
         </b-col>
+      </b-row>
+      <b-row id="withoutItems"
+             class="mt-3">
+        <div id="warn"
+             v-if="selected.length === 0">
+          Выберите мат. ценности
+        </div>
+        <b-button block
+                  v-else
+                  variant="success">
+          Скачать шаблон
+        </b-button>
       </b-row>
     </b-container>
   </b-modal>
@@ -68,17 +82,23 @@ export default {
       async addDoc() {
         debugger
         const response = await fetch(`${process.env.ROOT_API}/inventorybase/api/v1/docs/`, {
-            method: 'POST',
-            mode: 'cors',
-            headers: {
-              'Content-Disposition': 'attachment; filename=' + this.file.name,
-            },
-            body: this.file
-          })
-          if (response.status !== 201) {
-            alert(JSON.stringify(await response.json(), null, 2));
-          }
-
+          method: 'POST',
+          mode: 'cors',
+          headers: {
+            'Content-Disposition': 'attachment; filename=' + this.file.name,
+          },
+          body: this.file
+        })
+        if (response.status !== 201) {
+          alert(JSON.stringify(await response.json(), null, 2));
+        } else {
+          bus.$emit('updateDocList')
+        }
+      },
+      clear(){
+        this.doc = null
+        this.file = null
+        this.merge_docs = false
       }
     },
     watch:{
@@ -90,10 +110,22 @@ export default {
     },
     async created(){
       await this.fetchDocs()
+      await bus.$on('updateDocList', () => this.fetchDocs())
     }
   }
 </script>
 
 <style scoped>
-
+  #withoutItems {
+    background: #f6cdd1;
+    -webkit-border-radius: 5px;
+    -moz-border-radius: 5px;
+    border-radius: 5px;
+    min-height: 38px;
+  }
+  #warn{
+    text-align: center;
+    margin: auto;
+    vertical-align: center;
+  }
 </style>
